75fa06d36454a100a9d298c7d1e9f06aa0102196
AbdelhayZaadaddi <abdezaadaddi06@gmail.com>
2023-09-26 21:55:01 +0100

    Fexed error

--- Variables/Grade_Calculator.dart
+++ Variables/Grade_Calculator.dart
@@ -1,3 +1,23 @@
 void main(){
-  var test_score = 
+  var test_score = 5;
+  grade_calculator(test_score);
+}
+
+
+void grade_calculator(int score){
+  if (score >= 90 && score <= 100){
+    print("A");
+  }
+  else if (score >= 80 && score <= 89){
+    print("B");
+  }
+  else if (score >= 70 && score <= 79){
+    print("C");
+  }
+  else if (score >= 60 && score <= 69){
+    print("D");
+  }
+  else if (score < 60){
+    print("F");
+  }
 }
