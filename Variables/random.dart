7a97151121a20da75871eaad0e0dfcb10108924c
AbdelhayZaadaddi <abdezaadaddi06@gmail.com>
2023-09-26 22:02:51 +0100

    random

--- Variables/random.dart
+++ Variables/random.dart
@@ -4,5 +4,7 @@ void main(){
   // create the random object
   Random random = Random();
 
-  //
+  // generate a random integer between 1 and 100
+  int randomNumber = random.nextInt(100) + 1;
+  print("Random Integer: $")
 }
