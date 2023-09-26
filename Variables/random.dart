2e828efccab7501845ba04bea5dcd5d2a1766002
AbdelhayZaadaddi <abdezaadaddi06@gmail.com>
2023-09-26 22:05:22 +0100

    random try

--- Variables/random.dart
+++ Variables/random.dart
@@ -6,5 +6,9 @@ void main(){
 
   // generate a random integer between 1 and 100
   int randomNumber = random.nextInt(100) + 1;
-  print("Random Integer: $")
+  print("Random Integer: $randomNumber");
+
+  // generate a random double between 0.0 (inclusive) and 1.0 (exclusive)
+  double randomDouble = random.nextDouble();
+  print("Random Double:$randomDouble");
 }
