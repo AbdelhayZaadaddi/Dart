b2c3690dd87b3625a482c0dafda2657319d36472
AbdelhayZaadaddi <abdezaadaddi06@gmail.com>
2023-09-27 09:44:31 +0100

    switch

---
+++ 01_Variables/switch_case.dart
@@ -0,0 +1,15 @@
+void main(){
+  // Swich Case Statements: Applicable for only 'int' and 'string'
+  String grad = 'A';
+  switch (grad){
+    case 'A':
+      print("Execellent grade of A");
+    case 'B':
+      print('Very good!');
+    case 'C':
+      print('Good enough but work hard');
+      break;
+    default:
+      print("You have failed");
+  }
+}
