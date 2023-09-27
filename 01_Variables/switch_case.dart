dde55b392f9cc969c54435fde8070a442827c698
AbdelhayZaadaddi <abdezaadaddi06@gmail.com>
2023-09-27 09:47:20 +0100

    switch

--- 01_Variables/switch_case.dart
+++ 01_Variables/switch_case.dart
@@ -4,12 +4,15 @@ void main(){
   switch (grad){
     case 'A':
       print("Execellent grade of A");
+      break;
     case 'B':
       print('Very good!');
+      break;
     case 'C':
       print('Good enough but work hard');
       break;
     default:
       print("You have failed");
+      break;
   }
 }
