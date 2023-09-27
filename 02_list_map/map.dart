3edfd8a3545d40c4c47801c2f51e422825634c5a
AbdelhayZaadaddi <abdezaadaddi06@gmail.com>
2023-09-27 19:56:54 +0100

    map

--- 02_list_map/map.dart
+++ 02_list_map/map.dart
@@ -1,7 +1,7 @@
 void main(){
   // 1 Creating a Map
   // Using Map constructor
-  Map<String, int> ages = Map<String, int>();
+  //Map<String, int> ages = Map<String, int>();
 
   // Using map literal
   Map<String, String> fruit = {
