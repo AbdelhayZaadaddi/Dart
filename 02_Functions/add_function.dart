23410f14a88d05885529b87243fd48b5bcbbe2fd
AbdelhayZaadaddi <abdezaadaddi06@gmail.com>
2023-09-27 10:21:42 +0100

    sub function

--- 02_Functions/add_function.dart
+++ 02_Functions/add_function.dart
@@ -3,9 +3,15 @@ void main(){
   a = 10;
   b = 9;
   add_function(a, b);
+  sub_function(a, b);
 }
 
 void add_function(int a, int b){
   var result = a + b;
+  print(result);
+}
+
+void sub_function(int a, int b){
+  var result = a - b;
   print(result);
 }
