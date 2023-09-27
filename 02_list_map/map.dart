657d3318b0f169d38d9b48023b1acade8cb72171
AbdelhayZaadaddi <abdezaadaddi06@gmail.com>
2023-09-27 19:28:25 +0100

    map

---
+++ 02_list_map/map.dart
@@ -0,0 +1,12 @@
+void main(){
+  // 1 Creating a Map
+  // Using Map constructor
+  Map<String, int> ages = Map<String, int>();
+
+  // Using map literal
+  Map<String, String> fruit = {
+    'apple': 'red',
+    'banana': 'yellow',
+    'cherry': 'red',
+  };
+}
